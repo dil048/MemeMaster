fa14a5cf11b6989eb9f9519be16de5f72fd7bcca
DIan <dian@dians-mbp-2.lan>
2018-12-07 10:49:09 -0800

    profile update

--- public/js/controller/profilePageController.js
+++ public/js/controller/profilePageController.js
@@ -1,12 +1,32 @@
+import {changePassword} from "../firebase/firebaseActions.js"
+import {authentication} from "../firebase/credentials.js"
+
 let changePwBtn = document.getElementById("changePwBtn");
 let uploadBtn = document.getElementById("uploadBtn");
+let changePwForm = document.createElement("form");
+var user = authentication.currentUser;
+var name, email, photoUrl, uid, emailVerified;
+if(user) {
+    name = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    emailVerified = user.emailVerified;
+    uid = user.uid;
+} else {
+    console.log("user not signed in");
+}
+window.onload = () => {
+    nameText = document.getElementById("usernameTag");
+    nameText[0].innerHTML = "Username: " + user.displayName;
+};
 
-changePwBtn.onclick = () =>{
-    changePassword();
+changePwBtn.onclick = () => {
+    changePassword(user);
+
 }
 
 
 
-uploadBtn.onclick = () =>{
-    
+uploadBtn.onclick = () => {
+
 }
--- public/js/firebase/firebaseActions.js
+++ public/js/firebase/firebaseActions.js
@@ -4,7 +4,7 @@ function createAccount(email, password, callback){
   authentication.createUserWithEmailAndPassword(email,password).then((res)=>{
     let profileName = email.substring(0,email.indexOf('@'));
     console.log(res.user.uid);
-    db.ref('profile/' + res.user.uid).set({
+    db.ref('diaplayName/' + res.user.uid).set({
       profile: profileName,
       email: email,
       userId: res.user.uid
@@ -22,9 +22,14 @@ function signIn(email, password){
     console.log(error.message);
   })
 }
-function changePassword(){
-  console,log("int here");
-  console.log(authentication);
+function changePassword(user){
+  console.log(user);
+  authentication.sendPasswordResetEmail(user.email).then( () => {
+    // email sent
+  }).catch( (error) => {
+    alert("Sorry, an error happened, cannot send reset password email");
+    console.log(error);
+  })
 }
 
-export {createAccount,signIn, changePassword};
+export {createAccount,signIn,changePassword};
